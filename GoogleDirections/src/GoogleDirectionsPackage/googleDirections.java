cb0572967774c6245cdd301f5d4d518455460d15
Amritpalsingh091 <amritpal.singh091@gmail.com>
2016-06-18 10:28:08 -0400

    first commit

---
+++ GoogleDirections/src/GoogleDirectionsPackage/googleDirections.java
@@ -0,0 +1,10 @@
+package GoogleDirectionsPackage;
+
+public class googleDirections {
+
+	public static void main(String[] args) throws Exception {
+			new Methods().performSearch("1980, sherbrooke west", "1001, sherbrooke east, montreal", "driving");
+
+	}
+
+}
