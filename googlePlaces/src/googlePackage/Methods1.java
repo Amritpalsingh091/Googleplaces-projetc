eab71ee618c940c9455ae5f087ce307974f4d4cb
gurvinder15 <student@172.24.3.1>
2016-06-19 11:52:02 -0400

    hi second

--- googlePlaces/src/googlePackage/Methods1.java
+++ googlePlaces/src/googlePackage/Methods1.java
@@ -19,12 +19,12 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 
 public class Methods1 {
-	
-	
+
+
 	private static final String GOOGLE_API_KEY = "AIzaSyARiki0HBLlyR7xH0K3e4eifaSLzx8b-7E";
-	
+
 	private final HttpClient client = HttpClientBuilder.create().build();;
-	
+
 	public void performSearch(final String origins, final String destinations, final String mode) throws ParseException, IOException,
 	URISyntaxException{
 		final URIBuilder builder = new URIBuilder().setScheme("https").setHost("maps.googleapis.com").setPath("/maps/api/distancematrix/json");
@@ -37,44 +37,51 @@ public class Methods1 {
 		builder.addParameter("mode",mode);
 		builder.addParameter("key",GOOGLE_API_KEY);
 		final HttpUriRequest request = new HttpGet(builder.build());
-		System.out.print(request);
+		//System.out.print(request);
 		final HttpResponse execute = this.client.execute(request);
 		final String response = EntityUtils.toString(execute.getEntity());
-		System.out.println(response);
+		//System.out.println(response);
 
-		/*
-		 * JSONObject object_response = JSONObject.fromObject(response);
-		
+
+		// JSONObject object_response = JSONObject.fromObject(response);
+
 		JSONObject object = JSONObject.fromObject(response);
 
-		JSONArray routes_array = object.getJSONArray("routes");
+		JSONArray rows_array = object.getJSONArray("rows");
+		//System.out.println(rows_array);
 
-		for(int i = 0;i<routes_array.size();i++){
+		for(int i = 0;i<rows_array.size();i++){
+		
 
-			System.out.println("The route "+routes_array.size());
+			JSONObject route = rows_array.getJSONObject(i);
 
-			JSONObject route = routes_array.getJSONObject(i);
+			JSONArray elements_array = route.getJSONArray("elements");
 
-			JSONArray legs_array = route.getJSONArray("legs");
+			int j = 0;
 
-			System.out.println("The legs "+legs_array.size());
+			for(j =0;j<elements_array.size();j++){
 
-			for(int j =0;j<legs_array.size();j++){
+				JSONObject legs = elements_array.getJSONObject(j);
 
-				JSONObject legs = legs_array.getJSONObject(j);
-
-				String start = legs.getString("start_address");
-
-				System.out.println("The start address is: "+start);
-
-				String end = legs.getString("end_address");
-
-				System.out.println("The end address is: "+end);
+				JSONObject start = legs.getJSONObject("distance");
+				String value = start.getString("value");
+				System.out.println("The good distance  is: "+value);
+			   String array_distance[]= new String[3];
+			   
+					array_distance[j]= value;
+					
+					System.out.println(array_distance[j]);
+					if(array_distance[j] >= array_distance[j+1])
+					{
+						
+					}
+					
+			}
 			
 		
-	}
+			
 
-} */
+		} 
 
 
-}}
+	}}
